af88d21540e79f364dc585b2d6a7b380b1009cfb
drmax300 <drmax300@hotmail.com>
2021-06-19 02:14:40 +0000

    reverse alp ascii style

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main function
+ *Return: 0
+ *Description: if else while
+ */
+int main(void)
+{
+	int alp;
+	for (alp = 122; alp >= 97; alp--)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
