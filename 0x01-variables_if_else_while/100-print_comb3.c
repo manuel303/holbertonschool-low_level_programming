3a1504f29d5eaff7b63bc11ed8b9ba55ee799916
drmax300 <drmax300@hotmail.com>
2021-06-19 02:46:03 +0000

    it finished later advanced I am really tire..!

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+/* more header goes there */
+
+/* betty style for doc for function main goes there*/
+int main(void)
+{
+/*your code goes there*/
+	return (0);
+}
