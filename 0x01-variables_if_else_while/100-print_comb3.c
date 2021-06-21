d4cf34cd0d96b68e173c52a368d627a7d51ddc24
manuel <lreyes03@mail.com>
2021-06-21 09:34:41 +0000

    2 digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
-/* more header goes there */
+#include <stdio.h>
 
-/* betty style for doc for function main goes there*/
+/**
+ * main - Prints combination of numbers
+ *
+ * Return: Always (Success)
+ */
 int main(void)
 {
-/*your code goes there*/
+	int d1, d2;
+
+	for (d1 = '0'; d1 <= '9'; d1++)
+	{
+		for (d2 = '0'; d2 <= '9'; d2++)
+		{
+			if (d1 < d2)
+			{
+				putchar(d1);
+				putchar(d2);
+
+				if (d1 != '8' || (d1 == '8' && d2 != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
